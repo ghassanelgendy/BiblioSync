ee1e5b5134e75f53da5c2c12671c0603a249bf39
Rawan <rawanehab523@gmail.com>
2024-05-09 12:38:23 +0300

    fixed navbar display

--- backend/library/models.py
+++ backend/library/models.py
@@ -3,33 +3,35 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=255 , default=" Title gamed awy")
-    author = models.CharField(max_length=255 , default="os w rew")
-    cover = models.URLField(default= "https://m.media-amazon.com/images/I/51RTdGBiL6L._SY445_SX342_.jpg")
+    title = models.CharField(max_length=255, default=" Title gamed awy")
+    author = models.CharField(max_length=255, default="os w rew")
+    cover = models.URLField(
+        default="https://m.media-amazon.com/images/I/51RTdGBiL6L._SY445_SX342_.jpg"
+    )
     description = models.TextField(default="dah ktab")
-    published = models.CharField(max_length=255 , default="26 march 2004")
+    published = models.CharField(max_length=255, default="26 march 2004")
     isbn = models.CharField(max_length=13)
-    language = models.CharField(max_length=100 , default= " English")
-    pages = models.IntegerField(default= 354)
+    language = models.CharField(max_length=100, default=" English")
+    pages = models.IntegerField(default=354)
     genres = models.ManyToManyField(
         Genre
     )  # Use ManyToManyField for genres 3shan moken kaza genre
     availability = models.BooleanField(default=True)
     formats = models.TextField(default="['Paperback', 'eBook']")
-    borrowing_period = models.CharField(max_length=100 ,default="2 weeks")
+    borrowing_period = models.CharField(max_length=100, default="2 weeks")
     reservation_option = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
-    
-    class Meta: #elsatr dah by-sort elkotob ascendingly bel title
-        ordering = ['title']
-    
+
+    class Meta:  # elsatr dah by-sort elkotob ascendingly bel title
+        ordering = ["title"]
 
 
 class User(models.Model):
--- backend/library/views.py
+++ backend/library/views.py
@@ -3,53 +3,68 @@ from .models import Book
 import json
 from django.shortcuts import render, get_object_or_404
 
+
 def about_us(request):
-    return render(request, 'aboutus.html')
+    return render(request, "aboutus.html")
+
 
 def add_form(request):
-    return render(request, 'addform.html')
+    return render(request, "addform.html")
+
 
 def admin_profile(request):
-    return render(request, 'adminprofile.html')
+    return render(request, "adminprofile.html")
+
 
 def book_details(request, id):
     book = get_object_or_404(Book, id=id)
-    genres = book.genres.all() 
-    return render(request, 'bookdetails.html', {'book': book, 'genres': genres})
+    genres = book.genres.all()
+    return render(request, "bookdetails.html", {"book": book, "genres": genres})
+
 
 def change_password(request):
-    return render(request, 'change password.html')
+    return render(request, "change password.html")
+
 
 def collection(request):
-    return render(request, 'collection.html', {'books': Book.objects.all() })
+    return render(request, "collection.html", {"books": Book.objects.all()})
 
 
 def credits(request):
-    return render(request, 'credits.html')
+    return render(request, "credits.html")
+
 
 def edit_profile(request):
-    return render(request, 'Edit profile.html')
+    return render(request, "Edit profile.html")
+
 
 def edit_book(request):
-    return render(request, 'editbook.html')
+    return render(request, "editbook.html")
+
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, "login.html")
+
 
 def request_book(request):
-    return render(request, 'requestbook.html')
+    return render(request, "requestbook.html")
+
 
 def search_results(request):
-    return render(request, 'searchresults.html')
+    return render(request, "searchresults.html")
+
 
 def sign_up(request):
-    return render(request, 'signup.html')
+    return render(request, "signup.html")
+
 
 def todo(request):
-    return render(request, 'Todo.html')
+    return render(request, "Todo.html")
+
 
 def user_profile(request):
-    return render(request, 'userprofile.html')
+    return render(request, "userprofile.html")

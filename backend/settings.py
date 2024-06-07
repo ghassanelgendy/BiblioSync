1da46212a1bb3a06ef6528d5c63061b70b8f4510
Ghassan <112765677+ghassanelgendy@users.noreply.github.com>
2024-06-07 13:11:17 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -48,8 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        'whitenoise.middleware.WhiteNoiseMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
